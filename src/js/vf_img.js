0bebbf5edcbe644744494732ac2c6ff2fcdf1f4a
Dan Shahin <dan@codescience.com>
2014-12-09 13:37:19 -0800

    don't show broken images

--- src/js/vf_img.js
+++ src/js/vf_img.js
@@ -1,8 +1,13 @@
+//swap local urls for static resource urls
+//mapping is defined in $config.img
+
 $(document).ready(function(){
+    $(img).hide(); //hide all images (to prevent showing broken img)
     for(var img in $config.img){
         var src = $config.img[img];
         $(img).each(function(){
-            $(this).attr({src: $config.img[img]});
+            //swap in VF url for src atttribute and show img
+            $(this).attr({src: $config.img[img]}).show();
         });
     }
 })
